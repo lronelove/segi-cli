726157e41ecc2a384ba9fae1387a2570d504a7ba
lronelove <657828543@qq.com>
2020-07-13 16:39:58 +0800

    优化目录结构

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const chalk = require('chalk')
 const packageConfig = require('./package')
 const program = new commander.Command('segi-cli')
 
-const createApp = require('./src/create')
+const createApp = require('./src/commands/create')
 
 program
   .version('v' + packageConfig.version, '-v, --version')
   .description(packageConfig.desciption)
-  .option('-d, --detail', '', 'a tool to resolve crossing origin')
+  .option('-d, --detail', '', 'a tool to download template depend on what you want')
   .action(async (actionName, projectName) => {
     if (actionName === 'create') {
-	    createApp()
+	    createApp(projectName)
     }
   })
 program.parse(process.argv)
